6f590f8ba77cae722e2edddc42e526e681e398c2
coding4saudade <amanziyasuke@gmail.com>
2025-05-23 22:02:49 -0500

    update to homework post feedback and clean up

--- week3/week3homework.js
+++ week3/week3homework.js
@@ -1,39 +1,33 @@
-pizzaToppings = ["cheese", "sauce" , "sausage", "onion", "peperoni", "green onions"];
-
+const pizzaToppings = ["cheese", "sauce", "sausage", "onion", "peperoni", "green onions"];
 
 function greetCustomer(name) {
     const toppingsList = pizzaToppings.join(", ");
     console.log(`Greetings and welcome to pizza house ${name}! Our available toppings are: ${toppingsList}.`);
 }
 
-
-greetCustomer("Amanzi")
-
-function getPizzaOrder(quanity,size, crust, ...toppings){
-    console.log(`Pizza Ordered: ${quanity}, ${size}, ${crust}, ${toppings}`)
-    return[ quanity, size, crust, ...toppings]
+function getPizzaOrder(quantity, size, crust, ...toppings) {
+    const toppingsList = toppings.join(", "); 
+    console.log(`Order received: ${quantity} ${size} ${crust} pizza with the following toppings: ${toppingsList}.`);
+    return [quantity, size, crust, ...toppings];
 }
 
-
-function preparePizza([quanity, size, crust, ...toppings]){
-    console.log("..........cooking pizza.......")
+function preparePizza([quantity, size, crust, ...toppings]) {
+    console.log("..........cooking pizza.......");
     return {
-        quanity: quanity,
+        quantity: quantity,
         size: size,
         crust: crust,
         toppings: toppings
-    }; 
+    };
 }
 
-function servePizza(pizza){
-    console.log(`Order is up!  Here's your ${pizza.size} ${pizza.crust} pizza with  ${pizza.toppings}`)
-
-
+function servePizza(pizza) {
+    const toppingsList = pizza.toppings.join(", "); 
+    console.log(`Order is up! Here's your ${pizza.size} ${pizza.crust} pizza with ${toppingsList}. Enjoy!`);
 }
 
-let order = getPizzaOrder(1, "large", "thick crust", "cheese" , "more cheese", "onion");
-let pizza = preparePizza(order)
 
-servePizza(pizza)
-
-//console.log(pizza)
+greetCustomer("Amanzi");
+const customerOrder = getPizzaOrder(1, "large", "thick crust", "cheese", "more cheese", "onion");
+const pizza = preparePizza(customerOrder);
+servePizza(pizza);

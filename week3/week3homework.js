a3191eebb15f1c93fef591b451d6c867fbe42aec
coding4saudade <amanziyasuke@gmail.com>
2025-05-26 22:42:40 -0500

    removed join and added for loop to add pizza toppings

--- week3/week3homework.js
+++ week3/week3homework.js
@@ -1,12 +1,24 @@
 const pizzaToppings = ["cheese", "sauce", "sausage", "onion", "peperoni", "green onions"];
 
 function greetCustomer(name) {
-    const toppingsList = pizzaToppings.join(", ");
+    let toppingsList = "";
+    for (let i = 0; i < pizzaToppings.length; i++) {
+        toppingsList += pizzaToppings[i];
+        if (i < pizzaToppings.length - 1) {
+            toppingsList += ", ";
+        }
+    }
     console.log(`Greetings and welcome to pizza house ${name}! Our available toppings are: ${toppingsList}.`);
 }
 
 function getPizzaOrder(quantity, size, crust, ...toppings) {
-    const toppingsList = toppings.join(", "); 
+    let toppingsList = "";
+    for (let i = 0; i < toppings.length; i++) {
+        toppingsList += toppings[i];
+        if (i < toppings.length - 1) {
+            toppingsList += ", ";
+        }
+    }
     console.log(`Order received: ${quantity} ${size} ${crust} pizza with the following toppings: ${toppingsList}.`);
     return [quantity, size, crust, ...toppings];
 }
@@ -22,10 +34,15 @@ function preparePizza([quantity, size, crust, ...toppings]) {
 }
 
 function servePizza(pizza) {
-    const toppingsList = pizza.toppings.join(", "); 
+    let toppingsList = "";
+    for (let i = 0; i < pizza.toppings.length; i++) {
+        toppingsList += pizza.toppings[i];
+        if (i < pizza.toppings.length - 1) {
+            toppingsList += ", ";
+        }
+    }
     console.log(`Order is up! Here's your ${pizza.size} ${pizza.crust} pizza with ${toppingsList}. Enjoy!`);
 }
-
 
 greetCustomer("Amanzi");
 const customerOrder = getPizzaOrder(1, "large", "thick crust", "cheese", "more cheese", "onion");
